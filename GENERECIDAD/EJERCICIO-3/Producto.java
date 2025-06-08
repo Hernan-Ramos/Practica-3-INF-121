550dd8b0fe0e33056777d9bb51579af01bbf280e
Hernan-Ramos <diegohernanramoschoque@gmail.com>
2025-06-07 21:24:53 -0400

    Add files via upload

---
+++ GENERECIDAD/EJERCICIO-3/Producto.java
@@ -0,0 +1,11 @@
+public class Producto {
+    private String producto;
+    public Producto(String nombredelproducto){
+        this.producto = nombredelproducto;
+
+    }
+    public String toString(){
+        return " " + this.producto + " ";
+    }
+
+}
